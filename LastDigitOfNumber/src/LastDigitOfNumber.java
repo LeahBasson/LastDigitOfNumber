5045bfbd245102bf5cdfc08337deed392515f223
LeahBasson <leahbasson54@gmail.com>
2025-03-05 15:35:29 +0200

    :tada:

---
+++ LastDigitOfNumber/src/LastDigitOfNumber.java
@@ -0,0 +1,15 @@
+
+public class LastDigitOfNumber {
+
+    
+    public static void main(String[] args) {
+//        When you divide a number by 10
+        int num;
+        num = 404;
+        int lastDigit;
+        lastDigit = num % 10;
+        
+        System.out.println("Last digit of number: " + lastDigit);
+    }
+    
+}
