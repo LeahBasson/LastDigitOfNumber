87ddba52ab1030f98fa7cbe78ca62ac72c01eb8d
LeahBasson <leahbasson54@gmail.com>
2025-03-06 21:46:00 +0200

    :fire:

--- LastDigitOfNumber/src/LastDigitOfNumber.java
+++ LastDigitOfNumber/src/LastDigitOfNumber.java
@@ -1,12 +1,17 @@
 
+import java.util.Scanner;
+
+
 public class LastDigitOfNumber {
 
     
     public static void main(String[] args) {
-//        When you divide a number by 10
-        int num;
-        num = 404;
-        int lastDigit;
+        Scanner scn = new Scanner(System.in);
+//      Modulus is used to find the remainder. When you divide a number by 10. The remainder is the last digit.
+        int num,lastDigit;
+        
+        System.out.println("Enter number: ");
+        num = scn.nextInt();
         lastDigit = num % 10;
         
         System.out.println("Last digit of number: " + lastDigit);
